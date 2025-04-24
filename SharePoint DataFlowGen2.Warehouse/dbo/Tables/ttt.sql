7e2110670e6db9c8268fc4de6a7df774989a1b03
yahiro816 <admin@MngEnvMCAP820421.onmicrosoft.com>
2025-04-24 12:59:23 +0900

    Committing 16 items from workspace 174e37d3-5584-4fca-8a42-af3b38d00ed6

---
+++ SharePoint DataFlowGen2.Warehouse/dbo/Tables/ttt.sql
@@ -0,0 +1,10 @@
+CREATE TABLE [dbo].[ttt] (
+
+	[Source.Name] varchar(8000) NULL, 
+	[time] datetime2(6) NULL, 
+	[a] bigint NULL, 
+	[b] bigint NULL, 
+	[c] bigint NULL, 
+	[d] bigint NULL, 
+	[e] bigint NULL
+);
